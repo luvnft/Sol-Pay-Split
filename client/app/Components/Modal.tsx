a6d3f80b150bb6f90197c0478f2b8a8df41d89a9
mrb1nary <anubhab.techie@gmail.com>
2024-10-18 12:56:52 +0530

    Added functionality to add and delete receipents

---
+++ client/app/Components/Modal.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+}
+
+export const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+      <div className="bg-white rounded-lg p-6 w-1/3">
+        <button onClick={onClose} className="float-right text-black">X</button>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+                        
+
--- client/app/payment/page.tsx
+++ client/app/payment/page.tsx
@@ -6,14 +6,18 @@ import { useWallet, WalletContextState } from "@solana/wallet-adapter-react";
 import { Connection, LAMPORTS_PER_SOL, PublicKey, SystemProgram, clusterApiUrl } from "@solana/web3.js";
 import { AnchorProvider, Idl, Program, web3, BN } from "@coral-xyz/anchor";
 import idl from "../../../anchor/target/idl/payment_splitter.json";
-
+import { Modal } from "../Components/Modal";
+import { FaTrash } from "react-icons/fa";
 
 export default function Payment() {
   const wallet = useWallet();
   const [isMounted, setIsMounted] = useState(false);
   const [input, setInput] = useState("");
   const [isInitialized, setIsInitialized] = useState(false);
-  const [amount, setAmount] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [recipientPublicKey, setRecipientPublicKey] = useState("");
+  const [recipientAmount, setRecipientAmount] = useState("");
+  const [recipients, setRecipients] = useState<Map<string, number>>(new Map());
 
   type SolanaWallet = WalletContextState & {
     publicKey: PublicKey;
@@ -57,6 +61,14 @@ export default function Payment() {
     }
   };
 
+  const handleDeleteRecipient = (publicKey: string) => {
+    setRecipients(prev => {
+      const updatedRecipients = new Map(prev);
+      updatedRecipients.delete(publicKey);
+      return updatedRecipients;
+    });
+  };
+
   const handleInputChange = (e: any) => {
     const value = e.target.value;
     const regex = /^\d*\.?\d*$/;
@@ -65,35 +77,54 @@ export default function Payment() {
     }
   };
 
-  const handleDelete = async () => {
-  if (wallet.publicKey) {
-    try {
-      const [escrowPDA] = PublicKey.findProgramAddressSync(
-        [Buffer.from("escrow"), wallet.publicKey.toBuffer()],
-        program.programId
-      );
+  const handleRecipientPublicKeyChange = (e: any) => {
+    setRecipientPublicKey(e.target.value);
+  };
 
-      await program.methods
-        .delete()
-        .accounts({
-          escrowAccount: escrowPDA,
-          sender: wallet.publicKey,
-        })
-        .rpc();
-
-      console.log("Escrow account deleted!");
-      setIsInitialized(false); // Reset the initialized state
-    } catch (error) {
-      console.error("Error deleting escrow account:", error);
+  const handleRecipientAmountChange = (e: any) => {
+    const value = e.target.value;
+    const regex = /^\d*\.?\d*$/;
+    if (regex.test(value)) {
+      setRecipientAmount(value);
     }
-  } else {
-    console.log("Wallet not connected");
-  }
-};
+  };
 
+  const handleAddRecipient = () => {
+    if (recipientPublicKey && recipientAmount) {
+      setRecipients(prev => new Map(prev).set(recipientPublicKey, parseFloat(recipientAmount)));
+      setRecipientPublicKey("");
+      setRecipientAmount("");
+    }
+  };
+
+  const handleDelete = async () => {
+    if (wallet.publicKey) {
+      try {
+        const [escrowPDA] = PublicKey.findProgramAddressSync(
+          [Buffer.from("escrow"), wallet.publicKey.toBuffer()],
+          program.programId
+        );
+
+        await program.methods
+          .delete()
+          .accounts({
+            escrowAccount: escrowPDA,
+            sender: wallet.publicKey,
+          })
+          .rpc();
+
+        console.log("Escrow account deleted!");
+        setIsInitialized(false); // Reset the initialized state
+      } catch (error) {
+        console.error("Error deleting escrow account:", error);
+      }
+    } else {
+      console.log("Wallet not connected");
+    }
+  };
 
   const Initialize = async () => {
-    if (wallet.publicKey) {
+    if (wallet.publicKey && !isInitialized) {
       try {
         const totalAmount = new BN(Number(input) * web3.LAMPORTS_PER_SOL);
         const [escrowPDA] = PublicKey.findProgramAddressSync(
@@ -116,6 +147,14 @@ export default function Payment() {
         console.log(err);
       }
     }
+  };
+
+  const handleMakePaymentClick = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
   };
 
   if (!isMounted) {
@@ -144,12 +183,13 @@ export default function Payment() {
               className="w-full p-3 rounded-lg text-black focus:outline-none focus:ring-2 focus:ring-indigo-500 text-center"
               placeholder="Amount in SOL"
               value={input}
+              disabled={isInitialized}
               onChange={handleInputChange}
             />
             {input ? (
               <button
                 onClick={Initialize}
-                className="bg-gradient-to-r from-green-400 to-green-600 text-white px-4 py-2 mt-6 rounded-lg transition duration-300 hover:from-green-500 hover:to-green-700"
+                className="bg-gradient-to-r mx-5 from-green-400 to-green-600 text-white px-4 py-2 mt-6 rounded-lg transition duration-300 hover:from-green-500 hover:to-green-700 mb-4"
                 disabled={isInitialized}
               >
                 Initialize
@@ -160,13 +200,14 @@ export default function Payment() {
             {isInitialized && (
               <>
                 <button
-                  className="bg-gradient-to-r from-purple-400 to-purple-600 text-white px-4 py-2 mt-6 rounded-lg transition duration-300 hover:from-blue-500 hover:to-blue-700"
+                  onClick={handleMakePaymentClick}
+                  className="bg-gradient-to-r mx-5 from-purple-400 to-purple-600 text-white px-4 py-2 mt-6 rounded-lg transition duration-300 hover:from-blue-500 hover:to-blue-700 mb-4"
                 >
                   Make Payment
                 </button>
                 <button
                   onClick={handleDelete}
-                  className="bg-gradient-to-r from-red-400 to-red-600 text-white px-4 py-2 mt-6 rounded-lg transition duration-300 hover:from-red-500 hover:to-red-700"
+                  className="bg-gradient-to-r mx-5 from-red-400 to-red-600 text-white px-4 py-2 mt-6 rounded-lg transition duration-300 hover:from-red-500 hover:to-red-700"
                 >
                   Delete
                 </button>
@@ -177,6 +218,74 @@ export default function Payment() {
           <h4 className="text-white text-2xl font-bold">No Wallet Connected</h4>
         )}
       </div>
+
+      
+      <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
+        <h2 className="text-xl font-bold mb-4">Add Recipients</h2>
+        <input
+    type="text"
+    className="w-full p-3 mb-4 rounded-lg text-black focus:outline-none focus:ring-2 focus:ring-indigo-500"
+    placeholder="Recipient Public Key"
+    value={recipientPublicKey}
+    onChange={handleRecipientPublicKeyChange}
+  />
+  <input
+    type="number"
+    className="w-full p-3 mb-4 rounded-lg text-black focus:outline-none focus:ring-2 focus:ring-indigo-500"
+    placeholder="Amount in SOL"
+    value={recipientAmount}
+    onChange={handleRecipientAmountChange}
+  />
+  <div className="flex justify-end space-x-4">
+    <button
+      onClick={handleAddRecipient}
+      className="bg-gradient-to-r from-green-400 to-green-600 text-white px-4 py-2 rounded-lg transition duration-300 hover:from-green-500 hover:to-green-700"
+    >
+      Add
+    </button>
+    <button
+      className="bg-gradient-to-r from-blue-400 to-blue-600 text-white px-4 py-2 rounded-lg transition duration-300 hover:from-blue-500 hover:to-blue-700"
+    >
+      Confirm
+    </button>
+  </div>
+  <div className="mt-6 p-4 bg-gray-800 rounded-lg shadow-inner">
+    <h3 className="text-lg font-semibold text-white mb-2">Recipients List</h3>
+    {recipients.size > 0 ? (
+      <ul className="space-y-2">
+        {Array.from(recipients.entries()).map(([key, amount], index) => (
+          <li key={index} className="text-white flex justify-between items-center">
+            <div>
+              <span className="font-bold">Public Key:</span> {key} <br />
+              <span className="font-bold">Amount:</span> {amount} SOL
+            </div>
+            <button
+              onClick={() => handleDeleteRecipient(key)}
+              className="text-red-500 hover:text-red-700"
+            >
+              <FaTrash />
+            </button>
+          </li>
+        ))}
+      </ul>
+    ) : (
+      <p className="text-white">No recipients added yet.</p>
+    )}
+  </div>
+</Modal>
+
     </BackgroundLines>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
